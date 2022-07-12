7e05f3562f69338d1ccf8f6ed288fa00165db782
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-07-12 12:10:32 +0300

    Finalized task, added whole functionality

--- soldier.js
+++ soldier.js
@@ -1,5 +1,5 @@
 const soldier = {
-    
+
     name: 'J.Price',
     health: 10,
     weapon: {
@@ -8,9 +8,39 @@ const soldier = {
     },
     ammunition: 3,
 
-    fire: function () {},
-    reload: function () {},
-    injure: function () {},
+    fire: function () {
+        if(this.weapon.ammo > 0){
+            this.weapon.ammo -= 1
+            console.log("Fire! Fire! Don't move, drop the gun ! Fire!")
+        } else {
+            console.log("Mag is empty, reload quickly!")
+        }
+
+
+    },
+
+    reload: function () {
+        if(this.ammunition > 0) {
+            this.weapon.ammo = 30
+            this.ammunition -= 1
+            console.log('Reloading...')
+        } else {
+            console.log("There are no ammunition for reload...")
+        }
+ 
+    },
+
+    injure: function () {
+        if(this.health > 0){
+            this.health -= 1
+            console.log("I got shot, but i haven't give up!")
+        } else {
+            console.log('You dead...')
+            return false
+        }
+
+        
+    },
 
 
 }
