3bee34ed374266903f34d66e9a08a7f8f1a7b8f8
Waldgang <waldgang@MacBook-Air-salambek.local>
2022-07-12 11:56:43 +0300

    Added object soldier, also wrote all general propeties and methods

---
+++ soldier.js
@@ -0,0 +1,16 @@
+const soldier = {
+    
+    name: 'J.Price',
+    health: 10,
+    weapon: {
+        name: 'AK-47',
+        ammo: 30,
+    },
+    ammunition: 3,
+
+    fire: function () {},
+    reload: function () {},
+    injure: function () {},
+
+
+}
